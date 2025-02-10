44fb4ab75df726b93bc9c9e25af05026e4cd3063
Gustavo Lopes <gustavolopes@Gustavos-MacBook-Air.local>
2025-02-10 13:37:07 -0300

    chore: initial configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HealthcheckModule } from './healthcheck/healthcheck.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HealthcheckModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
