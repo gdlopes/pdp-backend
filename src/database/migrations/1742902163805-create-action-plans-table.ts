06f284a386342798cb246fd3a9316dc6935de9c9
Gustavo Lopes <gustavolopes@Gustavos-MacBook-Air.local>
2025-04-18 21:56:35 -0300

    chore: create migration for tasks table

---
+++ src/database/migrations/1742902163805-create-action-plans-table.ts
@@ -0,0 +1,135 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateActionPlansTable1742902163805 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'action_plans',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'user_id',
+            type: 'int',
+            isUnique: false,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'goal',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'alignment_with_life_career',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'motivation',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'current_level',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'expected_level',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'specific_goal',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'progress_tracking_method',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'resources',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'development_impact',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'estimated_completion_date',
+            type: 'timestamp',
+            isNullable: false,
+          },
+          {
+            name: 'learning_method',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'time_commitment',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'knowledge_application',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'rewards',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+      true,
+    );
+
+    await queryRunner.createForeignKey(
+      'action_plans',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('action_plans');
+    const foreignKey =
+      table &&
+      table.foreignKeys.find((fk) => fk.columnNames.indexOf('user_id') !== -1);
+    if (foreignKey) {
+      await queryRunner.dropForeignKey('action_plans', foreignKey);
+    }
+    await queryRunner.dropTable('action_plans');
+  }
+}
