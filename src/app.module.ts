f63aaa668095c03896d2df57799628dea4cecb45
Gustavo Lopes <gustavolopes@Gustavos-MacBook-Air.local>
2025-04-19 20:00:52 -0300

    feat: create users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { HealthcheckModule } from './healthcheck/healthcheck.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     HealthcheckModule,
     DatabaseModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
