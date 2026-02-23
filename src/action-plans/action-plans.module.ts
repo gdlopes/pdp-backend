b13c9010767338ea6bb577a22ea6ab63baed13f0
Gustavo Lopes <gustavolopes@Gustavos-MacBook-Air.local>
2026-02-23 07:59:02 -0300

    fix: action plan repository injection

--- src/action-plans/action-plans.module.ts
+++ src/action-plans/action-plans.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ActionPlansService } from './action-plans.service';
 import { ActionPlansController } from './action-plans.controller';
 import { UsersModule } from '../users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ActionPlansEntity from '../database/entities/action-plans.entity';
 
 @Module({
-  imports: [UsersModule],
   controllers: [ActionPlansController],
+  imports: [UsersModule, TypeOrmModule.forFeature([ActionPlansEntity])],
   providers: [ActionPlansService],
 })
 export class ActionPlansModule {}
